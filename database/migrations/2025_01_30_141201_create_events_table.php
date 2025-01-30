23ddf501a57a9bf2dcd9f0fa944c23703ca404d0
gavin <gavin.har@pm.me>
2025-01-30 10:44:33 -0500

    Initial commit

---
+++ database/migrations/2025_01_30_141201_create_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name');
+            $table->text('description');
+            $table->dateTime('starts_at');
+            $table->dateTime('ends_at');
+            $table->foreignId('location_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
